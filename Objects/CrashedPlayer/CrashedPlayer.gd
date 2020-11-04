c613e1d4f8e161476f0cf0abcfd144a61bcc5b3f
DaividFrank <DaividSavernin@outlook.com>
2020-11-04 23:04:57 +0200

    Add camera zoom out and in

--- Objects/CrashedPlayer/CrashedPlayer.gd
+++ Objects/CrashedPlayer/CrashedPlayer.gd
@@ -1,28 +1,31 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var walking_speed := 500
+export var falling_acc := 5
 
 var player : Node2D
 var drop := false
 var move_vec := Vector2()
+var camera_zoom := Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
+func on_start():
+	$Tween.interpolate_property($Camera2D, "zoom", camera_zoom, Vector2(1, 1), 1)
+	$Tween.start()
 
 func will_drop():
 	return is_on_wall() or is_on_ceiling()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_vec.x = 100
+	move_vec.x = walking_speed
 	if drop:
 		move_vec.x = 0
-		move_vec.y += 5
+		move_vec.y += falling_acc
 	move_and_slide(move_vec, Vector2.UP)
 	if get_slide_count() == 0 or will_drop():
 		drop = true
--- Objects/Player/Player.gd
+++ Objects/Player/Player.gd
@@ -8,11 +8,16 @@ const CrashedPlayer := preload("res://Objects/CrashedPlayer/CrashedPlayer.gd")
 # var a = 2
 # var b = "text"
 
-var thrust = Vector2.UP * 500
-var torque = 10000
+export var thrust = Vector2.UP * 500
+export var torque = 5000
 
 var crashed_player : CrashedPlayer = null
 var crashed := false
+
+export var camera_zoom_scale := 200
+export var min_camera_zoom := 1
+export var max_camera_zoom := 3
+var last_camera_zoom : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,16 +36,22 @@ func _integrate_forces(state):
 	if Input.is_action_pressed("ui_left"):
 		rotation_dir -= 1
 	applied_torque = rotation_dir * torque
+	var normalized_velocity = linear_velocity.length() / camera_zoom_scale
+	last_camera_zoom = $Camera2D.zoom
+	$Camera2D.zoom.x = min(max(min_camera_zoom, normalized_velocity), max_camera_zoom)
+	$Camera2D.zoom.y = min(max(min_camera_zoom, normalized_velocity), max_camera_zoom)
 
 func _on_Player_body_entered(body):
 	if body.is_in_group("Crash") and crashed_player == null:
 		crashed_player = CrashedPlayerScene.instance()
 		crashed_player.player = self
-		visible = false
 		crashed_player.global_position = global_position
+		crashed_player.camera_zoom = last_camera_zoom
 		get_parent().add_child(crashed_player)
+		crashed_player.on_start()
 		crashed = true
 		applied_force = Vector2(0, 0)
 		applied_torque = 0
 		rotation = 0
 		sleeping = true
+		visible = false
