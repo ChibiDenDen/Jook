ef93540d892d213a6e75b75f33b7704690b54825
Roy Moore <roy@moore.co.il>
2020-11-26 15:35:30 +0200

    Add Customize as Menu

--- Menus/Menu.gd
+++ Menus/Menu.gd
@@ -3,13 +3,16 @@ extends Control
 const UI := preload("res://Objects/UI/UI.gd")
 
 export var SettingsPath : NodePath
+export var CustomizePath : NodePath
 var Settings : Control
+var Customize: Control
 export var UiPath : NodePath
 var Ui : UI
 var player
 
 func _ready():
 	Settings = get_node_or_null(SettingsPath)
+	Customize = get_node_or_null(CustomizePath)
 	Ui = get_node_or_null(UiPath)
 	player = Ui.get_parent().get_node("Player/PlayerFly")
 	for item in $Menu.get_children():
@@ -36,7 +39,7 @@ func _on_pressed(text):
 		"Back":
 			change_menu(Ui.BackMenu)
 		"Customize":
-			pass
+			change_menu(Customize)
 		"Quit":
 			get_tree().quit()
 
